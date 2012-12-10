3e8b2d21d496f1b9ba718b17c2a9ca4f4b272d52
Chris Frisina <github@specialorange.org>
2012-12-10 03:09:25 -0500

    Coming together

--- Rails/fdxcm/app/models/fedex_contract.rb
+++ Rails/fdxcm/app/models/fedex_contract.rb
@@ -1,3 +1,6 @@
 class FedexContract < ActiveRecord::Base
   attr_accessible :content, :contractorContractName, :contractorContractNumber, :endDate, :fedexContractName, :fedexContractNumber, :startDate
+
+	has_many :employees
+	has_many :psas
 end

41429657d2b7c8d251f2cf6a8cf46ccc8acf6f68
Chris Frisina <github@specialorange.org>
2012-12-19 02:10:33 -0500

    heroku trial 4

--- db/migrate/20121214153938_change_employees_to_business_to_business_to_employee.rb
+++ db/migrate/20121214153938_change_employees_to_business_to_business_to_employee.rb
@@ -1,10 +1,11 @@
 class ChangeEmployeesToBusinessToBusinessToEmployee < ActiveRecord::Migration
   def up
-		remove_column :businesses, :employee_id
+		remove_column :businesses, :employees_id
 		add_column :employees, :business_id, :integer
   end
 
   def down
+    add_column :businesses, :employees_id, :integer
   	remove_column :employees, :business_id
   end
 end
